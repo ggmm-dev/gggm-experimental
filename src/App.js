8fb33966c06f07b215ed4da91e85877ba460a134
Tyler Hahn <tyler.hahn@gogeddit.com>
2019-01-04 12:40:46 -0600

    init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+
+import { Hero } from "ggmm-react-lib";
+import _ from "lodash";
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      admin: true,
+      hero: {
+        headline: "Headline",
+        subheadline: "Subheadline",
+        image: "https://source.unsplash.com/random",
+        type: "image",
+        overlay: true,
+        buttonUrl: "/",
+        height: "50",
+        buttonTitle: "Start"
+      }
+    };
+  }
+
+  renderAdmin = state => {
+    if (this.state.admin) {
+      return _.map(state, val => {
+        console.log(Object.keys(val));
+      });
+    }
+  };
   render() {
+    const hero = this.state.hero;
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <Hero
+          type={hero.type} //video or image
+          headline={hero.headline}
+          subheadline={hero.subheadline}
+          overlay={hero.overlay} //disables darkened overlay
+          buttonUrl={hero.buttonUrl}
+          height={hero.height} // represents percentage height
+          buttonTitle={hero.buttonTitle}
+          imageUrl={hero.image}
+        />
+        {this.renderAdmin(this.state.hero)}
       </div>
     );
   }
